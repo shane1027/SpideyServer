f6b9a3b3d6b66f77ec5f379b2ee7dd8fb257e611
Raelene Alvarez McDermott <ralvare5@nd.edu>
2017-05-01 16:13:20 -0400

    socket.c

--- socket.c
+++ socket.c
@@ -15,22 +15,51 @@
 /**
  * Allocate socket, bind it, and listen to specified port.
  **/
-int
-socket_listen(const char *port)
+int socket_listen(const char *port)
 {
-    struct addrinfo  hints;
+    struct addrinfo  hints = {
+    	.ai_family = AF_UNSPEC,
+    	.ai_socktype = SOCK_STREAM,
+    	.ai_flags = AI_PASSIVE,
+    };
+    
     struct addrinfo *results;
     int    socket_fd = -1;
+    int status;
+
+
+	// not sure what to use for host, currently NULL
 
     /* Lookup server address information */
+    if ((status = getaddrinfo(NULL, port, &hints, &results)) != 0) {
+    	fprintf(stderr, "Unable to lookup server information: %s\n", gai_strerror(status));
+    	return -1;
+    }
 
     /* For each server entry, allocate socket and try to connect */
     for (struct addrinfo *p = results; p != NULL && socket_fd < 0; p = p->ai_next) {
-	/* Allocate socket */
 
-	/* Bind socket */
-
+		/* Allocate socket */
+		if ((socket_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) {
+			fprintf(stderr, "Unable to make socket: %s\n", strerror(errno));
+			continue;
+		}
+		
+		/* Bind socket */
+		if (bind(socket_fd, p->ai_addr, p->ai_addrlen) < 0) {
+			fprintf(stderr, "Unable to bind: %s\n", strerror(errno));
+			close(socket_fd);
+			socket_fd = -1;
+			continue;
+		}
+		
     	/* Listen to socket */
+    	if (listen(socket_fd, SOMAXCONN) < 0) {
+    		fprintf(stderr, "Unable to listen: %s\n", strerror(errno));
+    		close(socket_fd);
+    		socket_fd = -1;
+    		continue;
+    	}
     }
 
     freeaddrinfo(results);
