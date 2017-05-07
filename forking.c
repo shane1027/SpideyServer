f5a761c00544c798b94b3cc80e64b57633fe898a
shane1027 <sryan8@nd.edu>
2017-05-07 01:26:45 -0400

    forking almost there

--- forking.c
+++ forking.c
@@ -5,6 +5,8 @@
 #include <errno.h>
 #include <signal.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include <unistd.h>
 
@@ -23,26 +25,37 @@ forking_server(int sfd)
     /* Accept and handle HTTP request */
     while (true) {
     	/* Accept request */
+        debug("about to accept request");
     	request = accept_request(sfd);
+        debug("accepted request");
     	if (request == NULL) { continue; }
 
 	/* Ignore children */
 		pid = fork();
 		if (pid < 0) {
 			fprintf(stderr, "Fork failed: %s", strerror(errno));
+                        //free_request(request);
+                        int status;
+                        waitpid(pid, &status, 0);
 			close(sfd);
 			continue;
 		}
-		else if (pid == 0) { //I am child
+		if (pid == 0) { //I am child
+                    debug("In child");
 			handle_request(request);
+                        debug("request handled");
+                        free_request(request);
+                        debug("request freed");
 			close(sfd);
+                        debug("closed sfd");
 			exit(0);
 		} else { 
+                        debug("got to else");
 			close(sfd); 
 		}
 
 	/* Fork off child process to handle request */
-		free_request(request);
+		//free_request(request);
     }
 
     /* Close server socket and exit*/
